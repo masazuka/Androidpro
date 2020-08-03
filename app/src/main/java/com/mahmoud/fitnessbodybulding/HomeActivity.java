e3faddee3f5721e8913bcec9990f066a247acdc1
masazuka <mahmoud.1009@hotmailcom>
2020-08-03 17:20:47 +0100

    initail Android Project

---
+++ app/src/main/java/com/mahmoud/fitnessbodybulding/HomeActivity.java
@@ -0,0 +1,31 @@
+package com.mahmoud.fitnessbodybulding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Thread thread = new Thread(){
+
+            public void run(){
+
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+    }
+}
